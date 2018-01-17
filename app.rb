f6d4d1a550555f2d25a38b93a934f2d4a5d45c2d
philb56 <phil.bugden@outlook.com>
2018-01-17 15:23:49 +0000

    enable sessions part complete

--- app.rb
+++ app.rb
@@ -2,17 +2,25 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
-  post '/names' do
-    # add names
-    @player1_name = params[:player1_name]
-    @player2_name = params[:player2_name]
+  get '/play' do
+    p session[:player1_name]#.inspect
+    @player1_name = session[:player1_name]#.inspect
+    @player2_name = session[:player2_name]#.inspect
+    p @player1_name
+    p @player2_name
     erb(:play)
+  end
 
-
+  post '/names' do
+    session[:player1_name] = params[:player1_name]
+    session[:player2_name] = params[:player2_name]
+    redirect to ('/play')
   end
 
 
