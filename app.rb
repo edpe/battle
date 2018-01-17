aab84170cc6d51b4db28b93d5f480608d1ed15d1
Ed Perkins <ed@edperkins.co.uk>
2018-01-17 14:36:16 +0000

    add player names

--- app.rb
+++ app.rb
@@ -3,8 +3,18 @@ require 'sinatra/base'
 class Battle < Sinatra::Base
 
   get '/' do
-    "Testing infrastructure working!"
+    erb(:index)
   end
+
+  post '/names' do
+    # add names
+    @player1_name = params[:player1_name]
+    @player2_name = params[:player2_name]
+    erb(:play)
+
+
+  end
+
 
   run! if app_file == $0
 end
--- spec/features/testing_infrastructure_spec.rb
+++ spec/features/testing_infrastructure_spec.rb
@@ -1,6 +1,9 @@
-feature 'Testing Infrastrucure' do
-  scenario 'Can run app and check page content' do
-    visit('/')
-    expect(page).to have_content 'Testing infrastructure working! '
+feature 'Enter names' do
+  scenario 'Expects players to fill in their names (in a form), submit that form, and see those names on-screen' do
+    visit ('/')
+    fill_in('player1_name', :with => 'Ed')
+    fill_in('player2_name', :with => 'Phil')
+    click_button 'Submit'
+    expect(page).to have_content 'Ed vs Phil'
   end
 end
